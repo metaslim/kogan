f22d69dbb01affb6182951f280e7794709aa4a33
Welly <welly@Wellys-MacBook-Pro.local>
2018-01-16 16:52:22 +1100

    test rest client

--- spec/lib/clients/simple_rest_client_spec.rb
+++ spec/lib/clients/simple_rest_client_spec.rb
@@ -2,9 +2,24 @@ require_relative '../../../lib/clients/simple_rest_client'
 
 describe "result" do
   it "raise_exception" do
-    base_url = "http://wrongurlfortesting.com.au"
+    base_url = "http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com"
+    WebMock.allow_net_connect!
     expect {
-      Kogan::Clients::SimpleRestClient.new(base_url).get("/api/products/1").body
-    }.to raise_exception(Exception)
+      VCR.turned_off {
+        Kogan::Clients::SimpleRestClient.new(base_url).get("/api/products/100")
+      }
+      WebMock.disable_net_connect!
+    }.to raise_exception(RestClient::NotFound)
+  end
+
+  it "raise_exception" do
+    base_url = "http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com"
+    WebMock.allow_net_connect!
+    expect {
+      VCR.turned_off {
+        Kogan::Clients::SimpleRestClient.new(base_url).get("/api/products/1").code
+      }
+      WebMock.disable_net_connect!
+    }.not_to raise_exception
   end
 end
